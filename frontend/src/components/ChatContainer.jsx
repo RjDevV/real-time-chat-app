43981cf941a21c765bd30bc09ad80255809fc2c2
Ranjan <steinrj444@gmail.com>
2025-12-24 19:41:14 +0530

    Fix color of chat bubble of receiver & senders

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -295,7 +295,13 @@ const ChatContainer = ({ call }) => {
               </time>
             </div>
 
-            <div className="chat-bubble flex flex-col">
+            <div
+              className={`chat-bubble flex flex-col ${
+                message.senderId === authUser._id
+                  ? "bg-primary text-primary-content"
+                  : "bg-base-200 text-base-content"
+              }`}
+            >
               {message.image && (
                 <img
                   src={message.image}
